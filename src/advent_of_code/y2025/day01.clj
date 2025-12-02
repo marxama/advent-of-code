8409d664dd32d721667ebd59c233684897957d78
Marcus Magnusson <marcus.wangnusson@volvocars.com>
2025-12-02 19:13:16 +0100

    day 1, part 2

--- src/advent_of_code/y2025/day01.clj
+++ src/advent_of_code/y2025/day01.clj
@@ -6,20 +6,52 @@
         steps (Integer/parseInt (subs rotation 1))]
     (* sign steps)))
 
-(defn turn [current rotation]
+(defn turn [position rotation]
   (-> rotation
       parse-rotation
-      (+ current)
-      (mod 100)))
+      (+ position)))
 
 (defn turn-all [initial rotations]
-  (reductions turn initial rotations))
+  (reductions (comp #(mod % 100) turn) initial rotations))
 
 (defn calculate-password [rotations]
   (->> rotations
-       (turn-all 0)))
+       (turn-all 50)
+       (filter zero?)
+       count))
 
 (defn day01_1 []
-  (-> "y2025/day01_example"
+  (-> "y2025/day01"
       util/read-resource-lines
       calculate-password))
+
+(defn sign [x]
+  (cond
+    (pos? x) 1
+    (neg? x) -1
+    :else 0))
+
+(defn turn-all-0x434C49434B [initial rotations]
+  (reductions
+   (fn [[position _] rotation]
+     (let [new-position (turn position rotation)]
+       [(mod new-position 100)
+        (+ (Math/abs (int (/ new-position 100)))
+           ; Below handles the case where we cross zero in the negative direction
+           ; (e.g. from 1 to -1, or 1 to 0)
+           (if (and (pos? position) (not (pos? new-position)))
+             1
+             0))]))
+   [initial 0]
+   rotations))
+
+(defn calculate-password-0x434C49434B [rotations]
+  (->> rotations
+       (turn-all-0x434C49434B 50)
+       (map second)
+       (apply +)))
+
+(defn day01_2 []
+  (-> "y2025/day01_example"
+      util/read-resource-lines
+      calculate-password-0x434C49434B))
