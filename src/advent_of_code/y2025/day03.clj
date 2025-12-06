244e2ae8fb9e38cfbe30cdefdd7468c07fdcab9f
Marcus Magnusson <marcus.wangnusson@volvocars.com>
2025-12-07 00:22:59 +0100

    day 3, part 2

--- src/advent_of_code/y2025/day03.clj
+++ src/advent_of_code/y2025/day03.clj
@@ -1,18 +1,34 @@
 (ns advent-of-code.y2025.day03
   (:require [advent-of-code.util :as util]))
 
-(defn max-joltage [line]
+(defn max-n-digit-joltage [line n]
+  (let [numbers (map #(Integer/parseInt (str %)) line)]
+    (loop [numbers numbers
+           n n
+           result []]
+      (if (zero? n)
+        (Long/parseLong (apply str result)) ; this is so lazy ;D
+        (let [max-num (apply max (take (- (count numbers) (dec n)) numbers))]
+          (recur
+           (drop 1 (drop-while #(not= % max-num) numbers))
+           (dec n)
+           (conj result max-num)))))))
+
+(defn max-2-digit-joltage [line]
   (let [numbers (map #(Integer/parseInt (str %)) line)
         max-first (apply max (drop-last numbers)) 
         max-second (apply max (drop 1 (drop-while #(not= % max-first) numbers)))]
     (+ (* 10 max-first) max-second)))
-
-(defn total-max-joltage [lines]
-  (reduce + (map max-joltage lines)))
 
 (defn parse-input []
   (util/read-resource-lines "y2025/day03"))
 
 (defn day03_1 []
   (->> (parse-input)
-       total-max-joltage))
+       (map max-2-digit-joltage)
+       (reduce +)))
+
+(defn day03_2 []
+  (->> (parse-input)
+       (map #(max-n-digit-joltage % 12))
+       (reduce +)))
