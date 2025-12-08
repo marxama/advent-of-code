33316446f8c9c14d5fab05cb33f3feb0919c77e3
Marcus Magnusson <marcus.wangnusson@volvocars.com>
2025-12-08 22:16:26 +0100

    day 6, part 2

--- src/advent_of_code/y2025/day06.clj
+++ src/advent_of_code/y2025/day06.clj
@@ -2,6 +2,13 @@
   (:require [advent-of-code.util :as util]
             [clojure.string :as string]))
 
+(defn parse-operator [operator]
+  (case operator
+    "*" *
+    "+" +
+    \* *
+    \+ +))
+
 (defn parse-input []
   (->> (util/read-resource-lines "y2025/day06")
        (map string/trim)
@@ -9,17 +16,36 @@
        (apply map vector) ; transpose
        (map reverse)
        (map (fn [[operator & operands]]
-              {:operator (case operator
-                           "*" *
-                           "+" +)
+              {:operator (parse-operator operator)
                :operands (map parse-long operands)}))))
+
+(defn execute-operations [operations]
+  (map (fn [{:keys [operator operands]}]
+         (apply operator operands))
+       operations))
 
 (defn day06_1 []
   (->> (parse-input)
-       (map (fn [{:keys [operator operands]}]
-              (apply operator operands)))
+       execute-operations
        (apply +)))
 
+(defn parse-input-part-2 []
+  (->> (util/read-resource-lines "y2025/day06")
+       (apply map str) ; transpose the characters rather than the numbers
+       (map string/trim)
+       (partition-by string/blank?)
+       (take-nth 2)
+       (map (fn [[operator-line :as lines]]
+              (let [operator-char (last operator-line)
+                    operator (parse-operator operator-char)
+                    operands (->> lines
+                                  (map #(re-find #"\d+" %))
+                                  (map parse-long))]
+                {:operator operator
+                 :operands operands})))))
+
+
 (defn day06_2 []
-  ; ohh...
-  )
+  (->> (parse-input-part-2)
+       execute-operations
+       (apply +)))
